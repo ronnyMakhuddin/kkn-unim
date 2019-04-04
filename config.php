f60a9b7e5ab289152eaddda7275d6caf22243bf5
ronnyMakhuddin <ronny8686@gmail.com>
2019-04-04 23:49:18 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
 
 	$host = "kkn-unimserver.database.windows.net";
-    $user = "dicoding";
-    $pass = "D1c0d1ng";
-    $db = "kkn-unim";
-    try {
-        $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
-        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-    } catch(Exception $e) {
-        echo "Failed: " . $e;
-    } 
+    	$user = "dicoding";
+    	$pass = "D1c0d1ng";
+    	$db = "kkn-unim";
+    	try {
+        	$conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
+        	$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    	} catch(Exception $e) {
+        	echo "Failed: " . $e;
+    	} 
  
 ?>
